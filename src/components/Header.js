58a1fc1a3479562d3c08c657bcf989f8a3c080ad
Frederick Ofori <fofori031@st.ug.edu.gh>
2024-07-01 00:55:05 +0000

    Added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Shop</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
